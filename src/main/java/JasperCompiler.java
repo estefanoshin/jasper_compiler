7f51688e6b5031e1e472cc1c9d76a6958409fbe9
estefano shin <estefano.shin@galenoargentina.com.ar>
2019-05-31 14:30:47 -0300

    updated for current projects with ireport 5.6.0

--- src/main/java/JasperCompiler.java
+++ src/main/java/JasperCompiler.java
@@ -11,28 +11,20 @@ import org.apache.commons.io.filefilter.TrueFileFilter;
 public class JasperCompiler {
 
 	public static void main(String[] args) throws JRException {
-		String currentPath = "/Users/eshin/workspace/";
-
-		// Get currently running directory
-		// String currentPath = System.getProperty(directory);
+		String currentPath = "/Users/eshin/workspace/mobile-rest-services/";
 
 		System.out.println("Current path is: " + currentPath);
 
-		// Go to directory where all the reports are
 		File rootDir = new File(currentPath);
 
 		// Get all *.jrxml files
 		Collection<File> files = FileUtils.listFiles(rootDir, new RegexFileFilter("^(.*\\.jrxml)"), TrueFileFilter.INSTANCE);
 
 		for (File file : files) {
-//			System.out.println("Compiling: " + file.getAbsolutePath());
-//			System.out.println("Output: " + file.getName() + ".jasper");
-			// Actual compiling
-			
-//			System.out.println(file.getAbsolutePath());
-
+			System.out.println("Compiling: " + file.getAbsolutePath());
 			JasperCompileManager.compileReportToFile(file.getAbsolutePath(), file.getAbsolutePath().replaceAll("jrxml", "jasper"));
-//			System.out.println("Compiling: completed!");
+
 		}
+		System.out.println("Compiling: completed!");
 	}
 }
